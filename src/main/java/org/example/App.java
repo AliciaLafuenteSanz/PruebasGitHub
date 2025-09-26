356b83caeec966cad35e37f4dcdeeb3fd774fa16
Alicia Lafuente <alicia.lafuente.sanz@alumnos.upm.es>
2025-09-26 14:15:35 +0200

    después del merge

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println("hola sofi");
         System.out.print("Hola Ali");
         System.out.print("Nueva rama");
+        System.out.print("Despues del merge");
     }
 }
